98e64f834691d5546cb8ebcdc3a00d8792088cc2
Edie <edie@Edies-MacBook-Pro.local>
2020-09-08 00:57:09 +0300

    Display all books library

--- script.js
+++ script.js
@@ -30,20 +30,28 @@ function formGrab() {
 let arr = []
  arr.push(document.getElementById("book-title").value);
  arr.push(document.getElementById("book-author").value);
- arr.push(document.getElementById("book-pages").value);
+ arr.push(Number.parseInt(document.getElementById("book-pages").value));
  arr.push(document.querySelector('input[name="status"]:checked').value);
- console.log(arr)
+ let book = new Book(...arr);
+ myLibrary = addBookToLibrary(book, myLibrary);
+ console.log(myLibrary)
 }
 
-let book1 = new Book( "josia", "josia auth", 10, "yes" );
+function displayBooks() {
+  for(i = 0; i < myLibrary.length; i++) {
+    document.write(myLibrary[i].title + ' ' + myLibrary[i].author + ' ' + myLibrary[i].pages + ' ' + myLibrary[i].read + "<br>");
+  }
+  //document.getElementById("submit").innerHTML = myLibrary;
+}
+
+
+let book1 = new Book( "Javascript Algorithm", "Josia", 10, "yes" );
 myLibrary = addBookToLibrary( book1, myLibrary );
 console.log( myLibrary[ 0 ] );
-let book2 = new Book( "edie", "edie auth", 10, "yes" );
+let book2 = new Book( "Ruby on Rails", "Edie",  20, "yes" );
 myLibrary = addBookToLibrary( book2, myLibrary );
 console.log( myLibrary[ 1 ] );
-let book3 = new Book( "test", "test auth", 10, "yes" );
+let book3 = new Book( "Oliver Twist", "Mark", 30, "no" );
 myLibrary = addBookToLibrary( book3, myLibrary );
 console.log( myLibrary[ 2 ] );
-removeBookToLibrary( 0, myLibrary );
-console.log( "after delete" );
-console.log( myLibrary );
+
