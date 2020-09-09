07b2c44949a85f3217aa6efe57895a98198b0d27
edieatha <edieatha@gmail.com>
2020-09-09 23:37:53 +0300

    More and more linter errors fixed

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(newbook, arrLibrary) {
   const newArrLibrary = arrLibrary.slice();
-  return myLibrary.concat( newbook );
+  return myLibrary.concat(newbook);
 }
 
 function removeBookToLibrary(index, arrLibrary) {
@@ -19,62 +19,58 @@ function removeBookToLibrary(index, arrLibrary) {
 Book.prototype.upRead = function () {
   if (this.read === 'Read? Yes') {
     this.read = 'Read? No';
-  }else {
+  } else {
     this.read = ' Read? Yes';
   }
 };
 
-const book1 = new Book('Javascript Algorithm', 'by Josia,', 10 + ' pages,', 'Read? Yes');
+const book1 = new Book('Javascript Algorithm', 'by Josia,', `${10} pages,`, 'Read? Yes');
 myLibrary = addBookToLibrary(book1, myLibrary);
-const book2 = new Book( 'Ruby on Rails', 'by Edie,',  20 + ' pages,', 'Read? Yes');
+const book2 = new Book('Ruby on Rails', 'by Edie,', `${20} pages,`, 'Read? Yes');
 myLibrary = addBookToLibrary(book2, myLibrary);
-const book3 = new Book( 'Oliver Twist', ' by Mark,', 30 + ' pages,', 'Read? No');
+const book3 = new Book('Oliver Twist', ' by Mark,', `${30} pages,`, 'Read? No');
 myLibrary = addBookToLibrary(book3, myLibrary);
 
-displayBooks( myLibrary );
+displayBooks(myLibrary);
 
 function formGrab() {
-  let form = document.getElementById( "myForm" );
-  let arr = [];
+  const form = document.getElementById('myForm');
+  const arr = [];
   let flag = false;
   let all = false;
-  arr.push( document.getElementById( "book-title" ).value );
-  arr.push("by " + ( document.getElementById( "book-author" ).value ) + ",");
-  arr.push( (Number.parseInt( document.getElementById( "book-pages" ).value )) + " pages, Read? " );
-  if ( document.contains( document.querySelector( 'input[name="status"]:checked' ) ) )  {
-    arr.push( document.querySelector( 'input[name="status"]:checked' ).value );
+  arr.push(document.getElementById('book-title').value);
+  arr.push(`by ${document.getElementById('book-author').value},`);
+  arr.push(`${Number.parseInt(document.getElementById('book-pages').value)} pages, Read? `);
+  if (document.contains(document.querySelector('input[name="status"]:checked'))) {
+    arr.push(document.querySelector('input[name="status"]:checked').value);
   } else {
-    arr.push( null );
+    arr.push(null);
   }
 
-  let book = new Book( ...arr );
+  const book = new Book(...arr);
 
-  flag = myLibrary.some( function( currentValue ) {
-    return currentValue.title == arr[ 0 ] && currentValue.author == arr[ 1 ];
-  } );
+  flag = myLibrary.some((currentValue) => currentValue.title == arr[0] && currentValue.author == arr[1]);
 
-  all = arr.every( function( currentValue ) {
-    return currentValue !== null && currentValue !== " " && currentValue !== NaN;
-  } );
+  all = arr.every((currentValue) => currentValue !== null && currentValue !== ' ' && currentValue !== NaN);
 
-  if ( flag || !all ) {
-    alert( "cannot clone book or all fields should not be empty" );
+  if (flag || !all) {
+    alert('cannot clone book or all fields should not be empty');
   } else {
-    myLibrary = addBookToLibrary( book, myLibrary );
-    console.log( myLibrary );
-    displayBooks( myLibrary );
+    myLibrary = addBookToLibrary(book, myLibrary);
+    console.log(myLibrary);
+    displayBooks(myLibrary);
     form.reset();
   }
 }
 
-function displayBooks( arrShow ) {
-  let displArr = arrShow.slice();
-  const container = document.querySelector( "#container" );
-  if ( document.contains( document.getElementById( "content" ) ) ) {
-    document.getElementById( "content" ).remove();
+function displayBooks(arrShow) {
+  const displArr = arrShow.slice();
+  const container = document.querySelector('#container');
+  if (document.contains(document.getElementById('content'))) {
+    document.getElementById('content').remove();
   }
-  const cardBody = document.createElement( "div" );
-  cardBody.setAttribute( "id", "content" );
+  const cardBody = document.createElement('div');
+  cardBody.setAttribute('id', 'content');
 
   for (let i = 0; i < displArr.length; i++) {
     const para = document.createElement('p');
@@ -87,13 +83,14 @@ function displayBooks( arrShow ) {
     del.addEventListener('click', () => {
       removeBookToLibrary(i, arrShow);
       displayBooks(myLibrary);
-     });
+    });
 
-     up.classList.add( 'update' );
-     up.textContent = 'Update status';
-     up.addEventListener('click', () => { displArr[i].upRead();
-     displayBooks(myLibrary);
-     });
+    up.classList.add('update');
+    up.textContent = 'Update status';
+    up.addEventListener('click', () => {
+      displArr[i].upRead();
+      displayBooks(myLibrary);
+    });
 
     para.innerHTML = `${displArr[i].title} ${displArr[i].author} ${displArr[i].pages} ${displArr[i].read} <br>`;
     cardBody.appendChild(para);
